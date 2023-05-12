ed16e9533c84cd5b8e724739c56603aa35d5e49c
lucas.mmarques.ext <lucas.mmarques.ext@panonline.com.br>
2023-05-11 22:50:34 -0300

    Refactor para navControler

--- app/src/main/java/com/omelhordobrasil/kotlinbr/tamojunto/lucao/fragmentsclass/MainActivity.kt
+++ app/src/main/java/com/omelhordobrasil/kotlinbr/tamojunto/lucao/fragmentsclass/MainActivity.kt
@@ -3,24 +3,28 @@ package com.omelhordobrasil.kotlinbr.tamojunto.lucao.fragmentsclass
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.NavHostFragment
+import com.omelhordobrasil.kotlinbr.tamojunto.lucao.fragmentsclass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHost.navController
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            val secondFragment = SecondFragment()
-            val fragmentsTransaction = supportFragmentManager.beginTransaction()
-            fragmentsTransaction.replace(R.id.fragmentContainerView2, secondFragment)
-            fragmentsTransaction.commit()
+//            val secondFragment = SecondFragment()
+//            val fragmentsTransaction = supportFragmentManager.beginTransaction()
+//            fragmentsTransaction.replace(R.id.fragmentContainerView2, secondFragment)
+//            fragmentsTransaction.commit()
+
+            navController.navigate(R.id.action_firstFragment_to_secondFragment)
         }
 
-        val firstFragment = FirstFragment.newInstance("Lucao", "Marques")
-        val fragmentsTransaction = supportFragmentManager.beginTransaction()
-        fragmentsTransaction.add(R.id.fragmentContainerView2, firstFragment)
-        fragmentsTransaction.commit()
     }
 }
